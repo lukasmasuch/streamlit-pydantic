50c32bad5c56128c1d695ba50f7470e66bbb8f89
Lukas Masuch <lukas.masuch@gmail.com>
2021-07-11 12:42:14 +0000

    Update demo app

--- examples/demo_app.py
+++ examples/demo_app.py
@@ -1,10 +1,13 @@
 import os
+import pathlib
 
 import streamlit as st
 
 from streamlit_pydantic.ui_renderer import name_to_title
 
-demos = [str(dir) for dir in filter(os.path.isfile, os.listdir(os.curdir))]
+path_of_script = pathlib.Path(__file__).parent.resolve()
+
+demos = [str(dir) for dir in filter(os.path.isfile, os.listdir(path_of_script))]
 
 for i, demo in enumerate(demos):
     st.write(name_to_title(demo))
