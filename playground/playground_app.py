dab8810a1193a9fe3d16946bbde8778f8bf24292
Lukas Masuch <lukas.masuch@gmail.com>
2021-07-11 12:52:08 +0000

    Move playground app to its own folder

--- playground/playground_app.py
+++ playground/playground_app.py
@@ -6,10 +6,13 @@ import streamlit as st
 from streamlit_pydantic.ui_renderer import name_to_title
 
 path_of_script = pathlib.Path(__file__).parent.resolve()
+path_to_examples = pathlib.Path(path_of_script).parent.joinpath("examples")
 
 demos = [str(dir) for dir in filter(os.path.isfile, os.listdir(path_of_script))]
 
 st.write(path_of_script)
 st.write(os.listdir(path_of_script))
+st.write(demos)
+st.write(path_to_examples)
 for i, demo in enumerate(demos):
     st.write(name_to_title(demo))
