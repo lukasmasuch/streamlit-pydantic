9037ac5c94f4d4769d92e1188ceab62d16101240
Lukas Masuch <lukas.masuch@gmail.com>
2021-07-11 13:47:52 +0000

    Improve example

--- examples/render_input.py
+++ examples/render_input.py
@@ -5,8 +5,8 @@ import streamlit_pydantic as sp
 
 class ExampleModel(BaseModel):
     some_text: str
-    some_number: int = 10
-    some_boolean: bool = True
+    some_number: int = 10  # Optional
+    some_boolean: bool = True  # Option
 
 
 input_data = sp.pydantic_input("model_input", ExampleModel, use_sidebar=True)
