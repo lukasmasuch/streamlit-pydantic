594c11f30292a3412bc5b47c38f715a5d6136533
Lukas Masuch <lukas.masuch@gmail.com>
2021-07-11 13:09:32 +0000

    Update playground

--- playground/playground_app.py
+++ playground/playground_app.py
@@ -6,9 +6,16 @@ import streamlit as st
 from streamlit_pydantic.ui_renderer import name_to_title
 
 path_of_script = pathlib.Path(__file__).parent.resolve()
-path_to_examples = pathlib.Path(path_of_script).parent.joinpath("examples")
+path_to_examples = pathlib.Path(path_of_script).parent.joinpath("examples").resolve()
 
-demos = [str(dir) for dir in filter(os.path.isfile, os.listdir(path_to_examples))]
+demos = []
+for example_file in os.listdir(path_to_examples):
+    file_path = path_to_examples.joinpath(example_file).resolve()
+
+    if not file_path.is_file():
+        continue
+
+    demos.append(str(file_path))
 
 st.write(path_of_script)
 st.write(os.listdir(path_to_examples))
