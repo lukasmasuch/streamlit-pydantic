a5d904d2fc2ace7aa2630a536578e7b72637376e
John Hille <47728752+HIL340@users.noreply.github.com>
2023-03-22 21:15:27 +0800

    Add the ability to pass in an instance to sp.pydantic_form() (#23)

--- examples/simple_form.py
+++ examples/simple_form.py
@@ -10,6 +10,20 @@ class ExampleModel(BaseModel):
     some_boolean: bool
 
 
-data = sp.pydantic_form(key="my_form", model=ExampleModel)
-if data:
-    st.json(data.json())
+from_model_tab, from_instance_tab = st.tabs(
+    ["Form inputs from model", "Form inputs from instance"]
+)
+
+with from_model_tab:
+    data = sp.pydantic_form(key="my_form", model=ExampleModel)
+    if data:
+        st.json(data.json())
+
+with from_instance_tab:
+    instance = ExampleModel(
+        some_number=999, some_boolean=True, some_text="instance text"
+    )
+
+    instance_input_data = sp.pydantic_form(key="my_form_instance", model=instance)
+    if instance_input_data:
+        st.json(instance_input_data.json())
